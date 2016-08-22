6c151ea097443e6a6dc588ffdf3c744eb5ae6665
dark.H <darkhackdevil@gmail.com>
2016-08-22 21:28:45 +0800

    change

--- static/js/init.js
+++ static/js/init.js
@@ -58,11 +58,87 @@ function initScoreTable() {
 }
 
 function initSelect() {
-  opts = '<option value="MODE">MODE</option>';
+  opts = '<option value="MODE" style="margin-left:10px">MODE</option>';
   MODES.forEach(function(m) {
     opts += '<option value="' + m + '">' + m + ' secs</option>';
   });
   $('#select-mode').html(opts);
+
+}
+
+function new_selector(){
+  var out = "";
+  var show = false;
+  d3.select("#select-mode")
+    .style({
+      'margin-left': '10px',
+      'padding-left': '10px'
+    })
+    .on("mouseover", function(){
+      if(!show){
+        d3.select("div.options")
+        .style({
+          position: "absolute",
+          width: "200px",
+          bottom: "20px"
+        })
+        .style("opacity","0")
+        .on("mouseout", function(){
+          if (out == "mode"){
+            
+          }
+        })
+        .selectAll("li")
+        .data(MODES)
+        .enter()
+        .append("li")
+        .append("button")
+        .style({
+          width:"100px",
+          height:"20px"
+        })
+        .attr("value", function(d){ return d})
+        .text(function(d){ 
+          show = true;
+          return d;
+        })
+        .on("click", function(){
+          if (this.value != 'MODE') {
+            timer['1'] = new Timer(1, parseInt(this.value));
+            timer['2'] = new Timer(2, parseInt(this.value));
+          }
+        });  
+
+        d3.select("div.options")
+          .attr("z-index", 33)
+          .transition()
+          .duration(800)
+          .style("opacity",1)
+          .transition()
+          .style("bottom", "80px");
+
+      }
+      
+
+
+        // .style("bottom")
+
+    })
+    .on("mouseout", function(){
+      out = "mode"
+      setTimeout(function(){
+        d3.select("div.options").selectAll("li")
+          .transition()
+          .duration(550)
+          .style("bottom", "0px")
+          .transition()
+          .style("opacity", "0")
+          .transition()
+          .remove(); 
+          show = false;
+      }, 14500)  
+      
+    })
 }
 
 $(document).ready(function() {
